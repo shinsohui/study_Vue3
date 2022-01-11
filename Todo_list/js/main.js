94d9efb5f9ff397bb3ba73c36ec52518a2d09289
shinsohui <ssh9601@dongguk.edu>
2022-01-11 19:02:23 +0900

    complete Todo_list function

--- Todo_list/js/main.js
+++ Todo_list/js/main.js
@@ -4,13 +4,17 @@ const list = document.querySelector(".list")
 
 const likeButtons = document.querySelectorAll(".like")
 
-likeButtons.forEach(like => {
-    like.addEventListener("click", ()=>{
-        console.log('clicked')
-    })
-})
+// likeButtons.forEach(like => {
+//     like.addEventListener("click", ()=>{
+//         console.log('clicked')
+//     })
+// })
 
-addButton.addEventListener("click", function(){
+function addItem(){
+    // 빈 내용일 때는 작성되지 못하게 하기 위함
+    // trim() 공백을 제거해주는 함수 -> 스페이스바를 치고 등록되는 것 방지
+    if(inputText.value.trim()  === "") return;
+
     //like
     const like = document.createElement("span");
     const likeIcon = document.createElement("i");
@@ -37,11 +41,48 @@ addButton.addEventListener("click", function(){
     manage.appendChild(clearIcon)
 
     const li = document.createElement("li");
+
+    //addEventListener를 동적으로 등록하기 위해 
+    //event : 하트 클릭 시
+    like.addEventListener("click", (e)=>{
+        //like의 target을 확인하기 위해 일단 e 출력
+        const target = e.target;
+
+        //삼항 연산자 사용하기
+        const text = target.innerText === "favorite" ? "favorite_border" : "favorite";
+        target.innerText = text;
+    })
+    // 체크 클릭 시 Todo check
+    checkIcon.addEventListener("click", (e)=>{
+        // console로 찍으면 parentNode의 또 다시 parentNode가 "li"
+        //인 것을 확인할 수 있음
+        const target = e.target.parentNode.parentNode;
+        target.classList.add("done");
+    })
+    // 클리어 클릭 시 삭제
+    clearIcon.addEventListener("click", (e)=>{
+        const target = e.target.parentNode.parentNode;
+        list.removeChild(target)
+    })
+
     li.appendChild(like)
     li.appendChild(item)
     li.appendChild(manage)
-
     list.appendChild(li)
+
+    inputText.value = "";
+
+    // 작성 후 텍스트 란에 커서가 맨 앞으로 움직이게 하기 위해
+    inputText.focus()
+
+}
+
+inputText.addEventListener("keypress", e =>{
+    if(e.keyCode === 13){
+        addItem()
+    }
 })
 
-console.log({inputText})
+
+addButton.addEventListener("click", addItem)
+
