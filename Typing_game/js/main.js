5f7b77a791c4ef6de977dd0d5cd5d67b2764ef92
shinsohui <ssh9601@dongguk.edu>
2022-01-11 20:24:52 +0900

    add random API

--- Typing_game/js/main.js
+++ Typing_game/js/main.js
@@ -4,16 +4,39 @@ const currentWord = document.querySelector("#current-word")
 const scoreDisplay = document.querySelector("#score")
 const timeDisplay = document.querySelector("#time")
 const messageDisplay = document.querySelector("#message")
+
 const GAME_TIME = 5;
+
+const API_URL = "https://random-word-api.herokuapp.com/word?number=100";
 
 let words = ["banana", "key", "car", "javascript", "sohui"]
 let score = 0
 let time = 0;
 let timeInterval;
 let isPlaying = false;
+let isReady = false;
+
+init()
+
+// function init(){
+//     //promise 문법
+//     //return해주는 내용을 res로 넘겨받고 then을 이용해 다시 넘긴다.
+//     //간결한 코드 사용을 위해 괄호, return 삭제
+//     // const res = fetch(API_URL).then((res)=> {return res.json()}).then((data)=>console.log(data));
+//     const res = fetch(API_URL).then(res => res.json()).then((data) => words = data);
+// }
+
+//async await (ES6 문법) 
+// 비동기 함수 callback과 promise의 단점을 보완하기 위해 추가됨
+async function init(){
+    // fecth가 다 실행된 후에 res에 담김
+    const res = await fetch(API_URL);
+    const data = await res.json();
+    words = data.filter(item => item.length < 7)
+    isReady = true;
+}
 
 time = GAME_TIME;
-
 
 wordInput.addEventListener("input", e =>{
     const typedText = e.target.value;
@@ -39,7 +62,7 @@ function countDown(){
     console.log(time)
     time = time -1;
     timeDisplay.innerText = time;
-    //time = 0 일 때는 끝
+    //time가 0 일 때는 끝
     if(time === 0){
         gameover();
     }
